3755186d292b969d8f764767480b41f130f870bf
Mitsunori Komatsu <komamitsu@gmail.com>
2025-11-27 22:53:43 +0900

    Minor optimization
    Reuse byte array in a special case

--- src/main/java/org/komamitsu/wormhole4j/ByteArray.java
+++ src/main/java/org/komamitsu/wormhole4j/ByteArray.java
@@ -68,7 +68,9 @@ final class ByteArray implements Comparable<ByteArray> {
   }
 
   ByteArray append(int x) {
-    // TODO: Reuse the bytes if `bytes[length + 1] == x`
+    if (bytes.length >= length + 1 && bytes[length] == ((byte) x)) {
+      return new ByteArray(bytes, length + 1);
+    }
     byte[] newBytes = new byte[length + 1];
     System.arraycopy(bytes, 0, newBytes, 0, length);
     newBytes[length] = (byte) x;
