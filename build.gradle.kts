1fdf83a246d903aad6203b5e52f69846b70ec63c
Mitsunori Komatsu <komamitsu@gmail.com>
2025-12-16 23:54:28 +0900

    Bump the version to 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
 }
 
 group = "org.komamitsu"
-version = "0.1.0"
+version = "0.2.0"
 
 val projectName = "Wormhole4j"
 val projectDescription = "High-performance ordered in-memory index for Java"
