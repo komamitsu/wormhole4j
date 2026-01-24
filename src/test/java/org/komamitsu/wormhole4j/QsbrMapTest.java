0c619ac13dfbf1c17e020304ac8d67cf52ed18fc
Mitsunori Komatsu <komamitsu@gmail.com>
2026-01-25 01:30:58 +0900

    Add tests for putAll()

--- src/test/java/org/komamitsu/wormhole4j/QsbrMapTest.java
+++ src/test/java/org/komamitsu/wormhole4j/QsbrMapTest.java
@@ -18,9 +18,7 @@ package org.komamitsu.wormhole4j;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
@@ -224,6 +222,230 @@ class QsbrMapTest {
     assertThat(map.getVersion()).isEqualTo(1);
 
     map.handleReadOperation(table -> assertThat(table.get(key)).isEqualTo(new Item(1, value)));
+  }
+
+  @Test
+  void separateSequentialPutAllAndGet_ShouldReturnProperValue() {
+    QsbrMap<Integer, Item> map = new QsbrMap<>();
+
+    assertThat(map.getVersion()).isEqualTo(0);
+
+    int key1 = getRandomInt();
+    int value1 = getRandomInt();
+    int key2 = getRandomInt();
+    int value2 = getRandomInt();
+
+    map.handleWriteOperation(
+        (version, table) -> {
+          Map<Integer, Item> src = new HashMap<>();
+          src.put(key1, new Item(version, value1));
+          src.put(key2, new Item(version, value2));
+          table.putAll(src);
+        });
+
+    assertThat(map.getVersion()).isEqualTo(1);
+
+    map.handleReadOperation(
+        table -> {
+          assertThat(table.get(key1)).isEqualTo(new Item(1, value1));
+          assertThat(table.get(key2)).isEqualTo(new Item(1, value2));
+        });
+  }
+
+  @Test
+  void singleGetModifyPutAllInTransaction_ShouldReturnProperValue() {
+    QsbrMap<Integer, Item> map = new QsbrMap<>();
+
+    assertThat(map.getVersion()).isEqualTo(0);
+
+    int key1 = getRandomInt();
+    int value1 = getRandomInt();
+    int key2 = getRandomInt();
+    int value2 = getRandomInt();
+
+    map.handleReadOperation(
+        readTable ->
+            map.handleWriteOperation(
+                (version, writeTable) -> {
+                  Map<Integer, Item> src = new HashMap<>();
+                  src.put(key1, new Item(version, value1));
+                  src.put(key2, new Item(version, value2));
+                  writeTable.putAll(src);
+                }));
+
+    assertThat(map.getVersion()).isEqualTo(1);
+
+    map.handleReadOperation(
+        table -> {
+          assertThat(table.get(key1)).isEqualTo(new Item(1, value1));
+          assertThat(table.get(key2)).isEqualTo(new Item(1, value2));
+        });
+  }
+
+  @Test
+  void multipleGetModifyPutAll_ShouldReturnProperValue() {
+    QsbrMap<Integer, Item> map = new QsbrMap<>();
+
+    assertThat(map.getVersion()).isEqualTo(0);
+
+    int key1 = getRandomInt();
+    int value1_1 = getRandomInt();
+    int value1_2 = getRandomInt();
+    int value1_3 = getRandomInt();
+    int key2 = getRandomInt();
+    int value2_1 = getRandomInt();
+    int value2_2 = getRandomInt();
+    int value2_3 = getRandomInt();
+
+    map.handleReadOperation(
+        readTable ->
+            map.handleWriteOperation(
+                (version, writeTable) -> {
+                  Map<Integer, Item> src = new HashMap<>();
+                  src.put(key1, new Item(version, value1_1));
+                  src.put(key2, new Item(version, value2_1));
+                  writeTable.putAll(src);
+                }));
+
+    assertThat(map.getVersion()).isEqualTo(1);
+
+    map.handleReadOperation(
+        readTable -> {
+          assertThat(readTable.get(key1)).isEqualTo(new Item(1, value1_1));
+          assertThat(readTable.get(key2)).isEqualTo(new Item(1, value2_1));
+          map.handleWriteOperation(
+              (version, writeTable) -> {
+                Map<Integer, Item> src = new HashMap<>();
+                src.put(key1, new Item(version, value1_2));
+                src.put(key2, new Item(version, value2_2));
+                writeTable.putAll(src);
+              });
+        });
+
+    assertThat(map.getVersion()).isEqualTo(2);
+
+    map.handleReadOperation(
+        readTable -> {
+          assertThat(readTable.get(key1)).isEqualTo(new Item(2, value1_2));
+          assertThat(readTable.get(key2)).isEqualTo(new Item(2, value2_2));
+          map.handleWriteOperation(
+              (version, writeTable) -> {
+                Map<Integer, Item> src = new HashMap<>();
+                src.put(key1, new Item(version, value1_3));
+                src.put(key2, new Item(version, value2_3));
+                writeTable.putAll(src);
+              });
+        });
+
+    assertThat(map.getVersion()).isEqualTo(3);
+
+    map.handleReadOperation(
+        table -> {
+          assertThat(table.get(key1)).isEqualTo(new Item(3, value1_3));
+          assertThat(table.get(key2)).isEqualTo(new Item(3, value2_3));
+        });
+  }
+
+  @Test
+  void getModifyPutAllWithConcurrentGet_ShouldReturnProperValue() throws InterruptedException {
+    QsbrMap<Integer, Item> map = new QsbrMap<>();
+
+    assertThat(map.getVersion()).isEqualTo(0);
+
+    int key1 = getRandomInt();
+    int value1 = getRandomInt();
+    int key2 = getRandomInt();
+    int value2 = getRandomInt();
+
+    CountDownLatch readBeforeModifyLatch = new CountDownLatch(1);
+    ExecutorService executorService = Executors.newCachedThreadPool();
+    executorService.execute(
+        () ->
+            map.handleReadOperation(
+                readTable -> {
+                  assertThat(readTable.get(key1)).isNull();
+                  assertThat(readTable.get(key2)).isNull();
+                  readBeforeModifyLatch.countDown();
+                  sleep(1);
+                  assertThat(readTable.get(key1)).isNull();
+                  assertThat(readTable.get(key2)).isNull();
+                }));
+
+    map.handleReadOperation(
+        readTable -> {
+          assertThat(readTable.get(key1)).isNull();
+          assertThat(readTable.get(key2)).isNull();
+          await(readBeforeModifyLatch);
+          map.handleWriteOperation(
+              (version, writeTable) -> {
+                Map<Integer, Item> src = new HashMap<>();
+                src.put(key1, new Item(version, value1));
+                src.put(key2, new Item(version, value2));
+                writeTable.putAll(src);
+              });
+        });
+
+    assertThat(map.getVersion()).isEqualTo(1);
+
+    executorService.shutdown();
+    assertThat(executorService.awaitTermination(2, TimeUnit.SECONDS)).isTrue();
+
+    map.handleReadOperation(
+        table -> {
+          assertThat(table.get(key1)).isEqualTo(new Item(1, value1));
+          assertThat(table.get(key2)).isEqualTo(new Item(1, value2));
+        });
+  }
+
+  @Test
+  void putAllWithConcurrentGet_ShouldNotBlockGet() throws InterruptedException {
+    QsbrMap<Integer, Item> map = new QsbrMap<>();
+
+    assertThat(map.getVersion()).isEqualTo(0);
+
+    int key1 = getRandomInt();
+    int value1 = getRandomInt();
+    int key2 = getRandomInt();
+    int value2 = getRandomInt();
+
+    CountDownLatch readBeforeModifyLatch = new CountDownLatch(1);
+    ExecutorService executorService = Executors.newCachedThreadPool();
+    executorService.execute(
+        () ->
+            map.handleReadOperation(
+                readTable -> {
+                  assertThat(readTable.get(key1)).isNull();
+                  assertThat(readTable.get(key2)).isNull();
+                  await(readBeforeModifyLatch);
+                  map.handleWriteOperation(
+                      (version, writeTable) -> {
+                        Map<Integer, Item> src = new HashMap<>();
+                        src.put(key1, new Item(version, value1));
+                        src.put(key2, new Item(version, value2));
+                        writeTable.putAll(src);
+                      });
+                }));
+
+    map.handleReadOperation(
+        readTable -> {
+          assertThat(readTable.get(key1)).isNull();
+          assertThat(readTable.get(key2)).isNull();
+        });
+
+    assertThat(map.getVersion()).isEqualTo(0);
+
+    readBeforeModifyLatch.countDown();
+
+    executorService.shutdown();
+    assertThat(executorService.awaitTermination(2, TimeUnit.SECONDS)).isTrue();
+
+    assertThat(map.getVersion()).isEqualTo(1);
+
+    map.handleReadOperation(
+        table -> {
+          assertThat(table.get(key1)).isEqualTo(new Item(1, value1));
+          assertThat(table.get(key2)).isEqualTo(new Item(1, value2));
+        });
   }
 
   @Test
