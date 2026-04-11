806d05819691918afe5070ab784092bb37e357eb
Mitsunori Komatsu <komamitsu@gmail.com>
2026-04-11 19:43:41 +0900

    Address license headers

--- src/main/java/org/komamitsu/wormhole4j/SimpleWormhole.java
+++ src/main/java/org/komamitsu/wormhole4j/SimpleWormhole.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Mitsunori Komatsu
+ * Copyright 2026 Mitsunori Komatsu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
